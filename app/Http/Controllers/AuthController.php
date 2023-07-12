40971b714f4e3cec697c6d47d80bec7d4b634f7e
JamiuKolawole04 <Jakko4real@gmail.com>
2023-07-12 11:07:39 +0100

    feat: laravel api done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
 use App\Models\User;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    use HttpResponses;
+    
     public function signup(SignupRequest $request) 
     {
         $request->validated($request->all());
@@ -22,6 +27,23 @@ class AuthController extends Controller
         return $this->success([
             "user" => $user,
             "token" => $user->createToken("API token of " . $user->name)->plainTextToken
-        ]);
+        ], "signup success");
+    }
+
+    public function login(LoginRequest $request)  
+    {
+        
+        $request-> validated($request->all());
+        if (!Auth::attempt($request->only(["email", "password"]))) {
+            return $this->error(null, "credentials do not match", 401);
+        }
+
+        $user = User::where("email", $request->email)->first();
+
+
+        return $this->success([
+            "user" => $user,
+            "token" => $user->createToken("API tojen of" . $user->name)->plainTextToken
+        ], 'login success');
     }
 }
