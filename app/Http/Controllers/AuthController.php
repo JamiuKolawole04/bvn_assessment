d9db27203c74caaeb47b3da6b613f74a506da6bd
JamiuKolawole04 <Jakko4real@gmail.com>
2023-07-12 10:21:05 +0100

    chore: adding traits

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function signup(SignupRequest $request) 
+    {
+        $request->validated($request->all());
+
+        $user = User::create([
+            "name" =>  $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
+        ]);
+        
+        return $this->success([
+            "user" => $user,
+            "token" => $user->createToken("API token of " . $user->name)->plainTextToken
+        ]);
+    }
 }
---
+++ app/Traits/HttpResponses.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Traits;
+
+// reusable code === trait
+trait HttpResponses {
+    protected function success($data, $message= null, $code = 200) {
+        return response()->json([
+            "status" => "Request was successful",
+            "message" => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    protected function error($data, $message= null, $code) {
+        return response()->json([
+            "status" => "Error has occured",
+            "message" => $message,
+            'data' => $data
+        ], $code);
+    }
+}
